644a5b54d92cb28b1bb38966e7f65ef66737ae44
yuanjilee <824528524@qq.com>
2018-01-26 14:28:03 +0800

    commit

--- VVDemo/ViewController.swift
+++ VVDemo/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var connectButton: UIButton!
+    
+    @IBAction func buttonDidClick(_ sender: UIButton) {
+        print("connect tap")
+        if(VpnManager.shared.vpnStatus == .off){
+            VpnManager.shared.connect()
+        }else{
+            VpnManager.shared.disconnect()
+        }
+    }
+
+
+    var status: VPNStatus {
+        didSet(o) {
+            updateConnectButton()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.status = .off
+        super.init(coder: aDecoder)
+        NotificationCenter.default.addObserver(self, selector: #selector(onVPNStatusChanged), name: NSNotification.Name(rawValue: kProxyServiceVPNStatusNotification), object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        connectButton.backgroundColor = .red
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.status = VpnManager.shared.vpnStatus
     }
-
-
+    
+    @objc func onVPNStatusChanged(){
+        self.status = VpnManager.shared.vpnStatus
+    }
+    
+    func updateConnectButton(){
+        switch status {
+        case .connecting:
+            connectButton.setTitle("connecting", for: UIControlState())
+        case .disconnecting:
+            connectButton.setTitle("disconnect", for: UIControlState())
+        case .on:
+            connectButton.setTitle("Disconnect", for: UIControlState())
+        case .off:
+            connectButton.setTitle("Connect", for: UIControlState())
+            
+        }
+        connectButton.isEnabled = [VPNStatus.on,VPNStatus.off].contains(VpnManager.shared.vpnStatus)
+        
+        
+    }
+    
 }
 
